130dd8f5fc3c98c577040d82ccd103e6da6a25f9
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-12-08 22:32:48 +0530

    Added contents

---
+++ blog-app/src/pages/AboutPage.jsx
@@ -0,0 +1,44 @@
+
+const AboutPage = () => {
+    return (
+        <>
+        <h1>About Mee</h1>
+        <p>
+            Welcome to my blog! 
+            Identifying myself as a logical and results-driven full-stack developer dedicated in building and optimizing user-focused websites and applications. 
+I love working in groups and can develop a rhythm with co-workers in no time.
+        </p>
+        <p>
+        • High knowledge on J2EE development toolset to design, test,
+deploy and maintain software.<br/>
+• Ability to manage both back-end and front-end aspects of
+development process.<br/>
+• Equipped with great coding, debugging and project
+management abilities.<br/>
+• Good understanding and hands on experience on Object
+Oriented Programming, Data structure and Algorithms,
+Version control - Git and GitHub<br/>
+• Deep view on Collections framework, REST, SOAP and RDBMS
+concepts.<br/>
+• A perfect team player with a zest for the enrichment of the
+organization and a well-organized, quick learner with the
+ability to plan and prioritize work.
+
+        </p>
+        <p>
+        An innovative optimized solution seeker and result-driven full-stack
+developer dedicated to build and optimize user-focused websites and
+applications.
+I love working in groups and can develop a rhythm with co-workers in
+no time.
+        </p>
+        <p>
+        As an Enthusiastic Fresher and a highly skilled Java Full Stack Developer
+        I have Mastered both Front-End and Back-End skills and I know I would make a valuable addition to the team
+        A perfect team player with a zest for the enrichment of the organization. Equipped with skills to handle pressure and go Hand-in-Hand with the team.
+        </p>
+    </>
+    );
+}
+
+export default AboutPage;
---
+++ blog-app/src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+
+const HomePage = () => (
+    <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+             
+            Identifying myself as a logical and results-driven full-stack developer dedicated in building and optimizing user-focused websites and applications. 
+I love working in groups and can develop a rhythm with co-workers in no time.
+        </p>
+        
+        <p>
+        An innovative optimized solution seeker and result-driven full-stack
+developer dedicated to build and optimize user-focused websites and
+applications.
+I love working in groups and can develop a rhythm with co-workers in
+no time.
+        </p>
+    </>
+);
+
+export default HomePage;
